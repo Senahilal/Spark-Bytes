f02ccbf8981dfb6994569bbcdc51bd2c1faa4bbb
Senahilal <senab@bu.edu>
2025-04-21 18:47:49 -0400

    Implemented accept/reject options and updated styling

--- spark-bytes/src/app/admin/page.tsx
+++ spark-bytes/src/app/admin/page.tsx
@@ -2,24 +2,22 @@
 
 import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
-import { Input, Dropdown, Space, Pagination, DatePicker, Select, ConfigProvider } from 'antd';
+import { Input, Dropdown, Space, Typography, Pagination, DatePicker, Select, ConfigProvider } from 'antd';
 import { SearchOutlined, DownOutlined } from '@ant-design/icons';
 import Logo from '../components/logo';
 import AccountIcon from '../components/accounticon';
 import ReqCard from '../components/reqcard';
 import Footer from '../components/footer';
 import Button from '../components/button';
-import dayjs, { Dayjs } from "dayjs";
+import dayjs from "dayjs";
 import { fetchAllRequests } from '../firebase/repository';
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 
-const { Search } = Input;
-const { Option } = Select;
+const { Title, Text } = Typography;
+
 
 export default function FindPage() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [currentPage, setCurrentPage] = useState(1);
 
   const [user] = useAuthState(auth);
 
@@ -30,11 +28,10 @@ export default function FindPage() {
   //fetching all events
   useEffect(() => {
     const loadRequests = async () => {
-        if (!user)
-        {
-            console.log("User not logged in");
-            return;
-        }
+      if (!user) {
+        console.log("User not logged in");
+        return;
+      }
       const fetchedRequests = await fetchAllRequests(user);
       if (fetchedRequests) {
         originalReqRef.current = fetchedRequests;
@@ -45,37 +42,10 @@ export default function FindPage() {
   }, [user]);
 
 
-  // !! Placeholders - change these !!
-  const events_placeholder = [
-    {
-      id: '1',
-      title: 'Cookie O\' Clock',
-      location: 'BU Spark',
-      date: '3/19',
-      time: '3pm',
-      foodType: 'Cookies',
-      hasNotification: true
-    },
-    {
-      id: '2',
-      title: 'Cookie O\' Clock',
-      location: 'BU Spark',
-      date: '3/19',
-      time: '3pm',
-      foodType: 'Cookies',
-      hasNotification: false
-    }
-  ];
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
   return (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-      {/* header */}
-      <div style={{ backgroundColor: '#DEEFB7', padding: '60px' }}>
+      {/* Header */}
+      <div style={{ backgroundColor: "#DEEFB7", padding: "24px" }}>
         <div style={{
           display: 'flex',
           justifyContent: 'space-between',
@@ -86,41 +56,27 @@ export default function FindPage() {
           <Link href="/">
             <Logo />
           </Link>
-
-          {/* post an event button */}
-          <div style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            gap: '10px'
-          }}>
-            <Button href="/create">
-              Post an Event
-            </Button>
-
-            <Link href="/profile">
-              <AccountIcon />
-            </Link>
-          </div>
+          <Link href="/profile">
+            <AccountIcon />
+          </Link>
         </div>
+        <div
+          style={{
+            maxWidth: "1024px",
+            margin: '80px auto 20px',
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Space>
 
-        <div style={{
-          maxWidth: '1200px',
-          margin: '40px auto 20px',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'flex-start'
-          // gap: '10px'
-        }}>
-          <div style={{
-            display: 'flex',
-            alignItems: 'center',
-            gap: '10px',
-            marginBottom: '30px'
-          }}></div>
-          <h1 style={{ fontSize: '28px', fontWeight: 'bold', margin: 0 }}>
-            Organizer Requests
-          </h1>
+            <div>
+              <Text strong style={{ fontSize: "28px" }}>
+              Organizer Requests
+              </Text>
+              </div>
+          </Space>
         </div>
       </div>
 
@@ -140,39 +96,32 @@ export default function FindPage() {
         ) : (
           <div style={{
             display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr))',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(900px, 1fr))',
             gap: '24px',
             marginBottom: '40px'
           }}>
             {requests.map(request => {
 
-            const start = request.created_at?.toDate?.();
-            const formattedDate = start ? dayjs(start).format("MM/DD/YYYY") : "Unknown Date";
-            const formattedTime = start ? dayjs(start).format("h:mm A") : "Unknown Time";
+              const start = request.created_at?.toDate?.();
+              const formattedDate = start ? dayjs(start).format("MM/DD/YYYY") : "Unknown Date";
+              const formattedTime = start ? dayjs(start).format("h:mm A") : "Unknown Time";
 
-            return (
-              <ReqCard
-                id={request.id}
-                user_name={request.user_name}
-                date={formattedDate}
-                time={formattedTime}
-                message={request.message}
-                status={request.status}
-              />
-            );
-          })}
+              return (
+                <ReqCard
+                  id={request.id}
+                  key={request.id}
+                  user_id={request.user_id}
+                  user_name={request.user_name}
+                  date={formattedDate}
+                  time={formattedTime}
+                  message={request.message}
+                  status={request.status}
+                />
+              );
+            })}
           </div>
         )}
 
-        {/* Pagination */}
-        <div style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}>
-          <Pagination
-            current={currentPage}
-            total={12}
-            onChange={handlePageChange}
-            pageSize={4}
-          />
-        </div>
       </div>
 
       <Footer />
