388cf40c6ad100523386ddcf6e9d08bd2ab7cf5b
Jose Amusategui <131242554+Periland@users.noreply.github.com>
2025-04-27 12:53:09 -0400

    Create page.tsx

---
+++ spark-bytes/src/app/page.tsx
@@ -0,0 +1,11 @@
+import dynamic from 'next/dynamic'
+
+// Dynamically import with no SSR
+const HomePage = dynamic(() => import('./HomePage'), { 
+  ssr: false,
+  loading: () => <div>Loading...</div>
+})
+
+export default function Page() {
+  return <HomePage />
+}
